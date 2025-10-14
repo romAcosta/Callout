0096599c1759a9bb211a8b35c2ec759c342060e6
romAcosta <161492657+romAcosta@users.noreply.github.com>
2025-10-13 20:11:18 -0600

    Vosk Started

---
+++ backend/recognizer.py
@@ -0,0 +1,16 @@
+import os
+
+import vosk
+import sounddevice as sd
+
+path = os.path.abspath("../models/vosk-model-small-en-us-0.15")
+model = vosk.Model(path)
+rec = vosk.KaldiRecognizer(model, 16000)
+
+with sd.RawInputStream(samplerate=16000, blocksize=8000, dtype='int16', channels=1) as stream:
+    while True:
+        data = stream.read(4000)[0]
+        if rec.AcceptWaveform(bytes(data)):
+            print(rec.Result())
+        else:
+            print(rec.PartialResult())
