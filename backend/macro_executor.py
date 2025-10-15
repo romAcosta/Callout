76f3692e0cffbea2a3b6e7223ad8b29133026549
romAcosta <161492657+romAcosta@users.noreply.github.com>
2025-10-15 10:29:34 -0600

    Macros Started

---
+++ backend/macro_executor.py
@@ -0,0 +1,10 @@
+from pynput.keyboard import Key, Controller
+
+
+keyboard = Controller()
+
+
+def execute_macro(command:str):
+    if(command.__contains__("reload")):
+        keyboard.press('a')
+    return
--- backend/recognizer.py
+++ backend/recognizer.py
@@ -1,16 +1,19 @@
+import json
 import os
 
 import vosk
 import sounddevice as sd
 
+from backend.macro_executor import execute_macro
+
 path = os.path.abspath("../models/vosk-model-small-en-us-0.15")
 model = vosk.Model(path)
-rec = vosk.KaldiRecognizer(model, 16000)
-
+rec = vosk.KaldiRecognizer(model, 16000,json.dumps(["open browser", "close window", "reload"]))
+rec.SetWords(['reload','next','hello'])
 with sd.RawInputStream(samplerate=16000, blocksize=8000, dtype='int16', channels=1) as stream:
     while True:
         data = stream.read(4000)[0]
         if rec.AcceptWaveform(bytes(data)):
             print(rec.Result())
-        if rec.Result().__contains__("hello"):
-            print("Heyo")
+
+        execute_macro(rec.Result())
